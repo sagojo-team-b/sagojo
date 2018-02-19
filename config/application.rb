5e1e31a3e529e38babad492bc17ca35f3e6f7087
稲員未来 <miku.inakazu@gmail.com>
2018-02-19 14:29:32 +0900

    don’t make assets, helper, coffee files when making controllers

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Sagojo
   class Application < Rails::Application
+    g.assets false
+    g.helper false
+    g.coffee false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
