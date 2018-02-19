887b1b6236ad615148f4f31eb3f5442e68299d4e
稲員未来 <miku.inakazu@gmail.com>
2018-02-19 14:41:15 +0900

    don’t make test files

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Sagojo
     g.assets false
     g.helper false
     g.coffee false
+    g.test   false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
