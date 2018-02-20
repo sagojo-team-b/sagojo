c1314524edab8d508b48246f5ee5a3f75fcc360b
Naoki Shinagawa <naoki.shina@gmail.com>
2018-02-20 13:42:29 +0900

    add column migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220022000) do
+ActiveRecord::Schema.define(version: 20180220035011) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
