a9827b8c1508eb2ee300f0200518420daccb960c
Naoki Shinagawa <naoki.shina@gmail.com>
2018-02-23 13:34:08 +0900

    add column convention n add strong parameter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220035011) do
+ActiveRecord::Schema.define(version: 20180223035835) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180220035011) do
     t.datetime "updated_at",                          null: false
     t.string   "name",                                null: false
     t.integer  "magazine",                            null: false
+    t.integer  "convention",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
