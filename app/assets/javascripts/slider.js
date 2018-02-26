937f9530591954b4dea1d6fba046295d8848e8cd
稲員未来 <miku.inakazu@gmail.com>
2018-02-26 18:39:27 +0900

    modify slider to implement slide function into home screen

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $('#slider').slick({
     centerMode: true,
-    centerPadding: '160px'
-  // accessibility: true,
-  // autoplay: true,
-  // autoplaySpeed: 1000,
-  // dots: true,
-  // fade: true,
+    centerPadding: '160px',
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1200,
+    dots: true,
   });
 });
 
