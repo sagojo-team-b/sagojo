6ca353920f7c90ae16de28e651a22f0bf03c4d62
稲員未来 <miku.inakazu@gmail.com>
2018-03-01 13:46:50 +0900

    modify indent of routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
   get 'home'          => 'header#home'
-  get 'home/job'      =>  'header#job'
+  get 'home/job'      => 'header#job'
   get 'home/triplab'  => 'header#triplab'
   get 'home/newcomer' => 'header#newcomer'
   get 'home/company'  => 'header#company'
