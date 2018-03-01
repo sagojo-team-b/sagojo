54b256c2ff1d64265dd529b68b29db97f593d4ed
稲員未来 <miku.inakazu@gmail.com>
2018-03-01 12:45:51 +0900

    made routing for home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'articles#index'
+  get 'home'          => 'header#home'
+  get 'home/job'      =>  'header#job'
+  get 'home/triplab'  => 'header#triplab'
+  get 'home/newcomer' => 'header#newcomer'
+  get 'home/company'  => 'header#company'
+  get 'home/FAQ'      => 'header#faq'
 end
