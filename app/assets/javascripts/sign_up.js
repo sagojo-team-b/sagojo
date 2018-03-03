b82ecbcc4a5a622449024908dfcd6d46a90239e9
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-03 20:17:04 +0900

    show the balloon when there is null inside the form

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -1,4 +1,5 @@
 $(function() {
+  // 規約利用のチェックボックスの要素を取得して、バルーンを表示
   $('.account-box__field__action__sign-in, .account-box__field__fb').css({opacity: "0.6", cursor: "defult"}).attr("disabled", "disabled");
   $('.convention-btn').attr('checked', false);
   $('.convention-btn').click(function (e) {
@@ -26,36 +27,82 @@ $(function() {
       })
     }
   })
-
-
-  $('#name').showBalloon({
-    position: "left",
-    contents: "<p>*必須項目です</p>",
-    css: {
-    backgroundColor: "red",
-    opacity: "0.6",
-    color: "white",
-    fontSize: "14px"
+// nameフォームの要素を取得して、空なら注意バルーン表示
+  $('#name').focusout(function(e) {
+    if(!$('#name').val().match(/\S/g)){
+      $('#name').showBalloon({
+        position: "left",
+        contents: "<p>*必須項目です</p>",
+        css: {
+          backgroundColor: "red",
+          opacity: "0.6",
+          color: "white",
+          fontSize: "14px"
+        }
+      });
+    } else {
+      $('#name').hideBalloon({
+        position: "left",
+        contents: "<p>*必須項目です</p>",
+        css: {
+          backgroundColor: "red",
+          opacity: "0.6",
+          color: "white",
+          fontSize: "14px"
+        }
+      });
     }
   });
-  $('#email').showBalloon({
-    position: "left",
-    contents: "<p>*必須項目です<br>*メールアドレスが正しくありません</p>",
-    css: {
-    backgroundColor: "red",
-    opacity: "0.6",
-    color: "white",
-    fontSize: "14px"
+//emailフォームの要素を取得して、空ならバルーン表示
+  $('#email').focusout(function(e) {
+    if(!$('#email').val().match(/\S/g)){
+      $('#email').showBalloon({
+        position: "left",
+        contents: "<p>*必須項目です<br>*メールアドレスが正しくありません</p>",
+        css: {
+          backgroundColor: "red",
+          opacity: "0.6",
+          color: "white",
+          fontSize: "14px"
+        }
+      });
+    } else {
+      $('#email').hideBalloon({
+        position: "left",
+        contents: "<p>*必須項目です<br>*メールアドレスが正しくありません</p>",
+        css: {
+          backgroundColor: "red",
+          opacity: "0.6",
+          color: "white",
+          fontSize: "14px"
+        }
+      });
     }
   });
-  $('#password').showBalloon({
-    position: "left",
-    contents: "<p>*必須項目です<br>*6文字以上にしてください</p>",
-    css: {
-    backgroundColor: "red",
-    opacity: "0.6",
-    color: "white",
-    fontSize: "14px"
+//passwordフォームの要素を取得して、空ならバルーン表示
+  $('#password').focusout(function(e) {
+    if(!$('#password').val().match(/\S/g)){
+      $('#password').showBalloon({
+        position: "left",
+        contents: "<p>*必須項目です<br>*6文字以上にしてください</p>",
+        css: {
+          backgroundColor: "red",
+          opacity: "0.6",
+          color: "white",
+          fontSize: "14px"
+        }
+      });
+    } else {
+      $('#password').hideBalloon({
+        position: "left",
+        contents: "<p>*必須項目です<br>*6文字以上にしてください</p>",
+        css: {
+          backgroundColor: "red",
+          opacity: "0.6",
+          color: "white",
+          fontSize: "14px"
+        }
+      });
     }
   });
 });
