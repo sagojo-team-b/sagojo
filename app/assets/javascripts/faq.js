ba547a42e7c5a8fabd5ffb97973eef4264809a0f
稲員未来 <miku.inakazu@gmail.com>
2018-03-06 14:56:29 +0900

    implement accordion menu and automatically scroll when push certain button

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -8,7 +8,16 @@ $(function() {
     var th = $('.faq_right_inner').children("#" + click).position().top;
     var header_height = $('#header-01').height();
 
-    $(window).scrollTop(th - header_height);
-    // $(window).animate({scrollTop: th - header_height}, 2000);
+    // $(window).scrollTop(th - header_height);
+    $("html, body").animate({scrollTop: th - header_height}, 300);
+  });
+
+  $('.for_company_list > li').on('click', function() {
+    var click = $(this).attr('id');
+    var th = $('.faq_right_inner').children("#" + click).position().top;
+    var header_height = $('#header-01').height();
+
+    // $(window).scrollTop(th - header_height);
+    $("html, body").animate({scrollTop: th - header_height}, 300);
   });
 });
