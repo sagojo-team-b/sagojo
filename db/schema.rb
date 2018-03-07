442fd98b96d3eb40ee6eedeed4ae5feab53c37b8
T.K <>
2018-03-07 14:29:54 +0900

    rails g article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223035835) do
+ActiveRecord::Schema.define(version: 20180307051543) do
+
+  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.text     "image_url",              limit: 65535
+    t.text     "introduction",           limit: 65535
+    t.text     "work_content",           limit: 65535
+    t.string   "reward"
+    t.text     "other_reward",           limit: 65535
+    t.text     "requirements",           limit: 65535
+    t.string   "num_people"
+    t.string   "travel_period"
+    t.string   "area"
+    t.string   "company"
+    t.text     "other",                  limit: 65535
+    t.text     "application_deadline",   limit: 65535
+    t.text     "responsible_party_icon", limit: 65535
+    t.text     "single_word",            limit: 65535
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
