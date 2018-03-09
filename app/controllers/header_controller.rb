bd6796261dab2b2378f86c34b65142e589f213da
T.K <>
2018-03-09 12:53:33 +0900

    configure routing sigoto detail

--- app/controllers/header_controller.rb
+++ app/controllers/header_controller.rb
@@ -7,6 +7,10 @@ class HeaderController < ApplicationController
     @articles = Article.page(params[:page]).per(10)
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def triplab
     @triplab_articles = TriplabArticle.page(params[:page]).per(10)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   root 'articles#index'
   get 'home',          to: 'header#home'
   get 'home/job',      to: 'header#job'
+  get 'home/job/:id',  to: 'header#show', as: 'home_job_detail'
   get 'home/triplab',  to: 'header#triplab'
   get 'home/newcomer', to: 'header#newcomer'
   get 'home/company',  to: 'header#company'
