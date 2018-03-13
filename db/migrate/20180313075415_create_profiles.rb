28431f5e04a302e8caedfa39d149eb8597771338
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-13 17:49:37 +0900

    create model profile

---
+++ db/migrate/20180313075415_create_profiles.rb
@@ -0,0 +1,39 @@
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+      t.references    :user, null: false
+      t.string        :avatar
+      t.string        :background_image
+      t.string        :catch_phrase
+      t.integer       :sex
+      t.integer       :tel
+      t.integer       :post_number
+      t.string        :address
+      t.string        :present_place
+      t.string        :private_link
+      t.integer       :pv
+      t.string        :sns
+      t.integer       :follower
+      t.integer       :job_type_tags_1
+      t.integer       :job_type_tags_2
+      t.integer       :job_type_tags_3
+      t.integer       :job_type_tags_4
+      t.text          :job_type_else
+      t.text          :language
+      t.text          :job_history
+      t.text          :travel_history
+      t.integer       :skill_tags_1
+      t.integer       :skill_tags_2
+      t.integer       :skill_tags_3
+      t.integer       :skill_tags_4
+      t.text          :skill_tags_else
+      t.text          :achivement
+      t.string        :income
+      t.string        :travel_date
+      t.text          :travel_content
+      t.text          :self_introduction
+
+      t.timestamps
+    end
+  end
+end
