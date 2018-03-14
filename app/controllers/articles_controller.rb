ed6279795c2c928f71f7726a2950b04d1c3251b7
T.K <>
2018-03-14 19:05:16 +0900

    add create_time method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.limit(6)
     @slider_articles = Article.limit(3)
+    @current_date = Date.current
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def create_time(application_deadline)
+    date = application_deadline.chomp
+    date = Date.strptime(application_deadline, '%Y年%m月%d日')
+    current_date = Date.current
+    (date - current_date).numerator
+  end
 end
