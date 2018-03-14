d637bd1946520d68c3213a7b8804d1af3189f9e6
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-14 19:16:18 +0900

    add form border color in js file

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  $('#email, #password, #name, #re-password, #email_session, #password_session').focusin(function(e) {
+  $('#email, #password, #name, #re-password, #email_session, #password_session, #avatar, #background_image, #catch_phrase, #sex, #tel, #post_number, #address, #present_place, #private_link, #pv, #sns, #follower, #job_type_tags_1, #job_type_tags_2, #job_type_tags_3, #job_type_tags_4, #job_type_else, #language, #job_history, #travel_history, #skill_tags_1, #skill_tags_2, #skill_tags_3, #skill_tags_4, #skill_tags_else, #achivement, #income, #travel_date, #travel_content, #self_introduction').focusin(function(e) {
     $(this).css('border', 'solid 2px #FFCC33');
   })
   .focusout(function(e) {
