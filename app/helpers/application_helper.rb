f62f38e36556a1fc119953d5b0a2fc7ff2807a6b
T.K <>
2018-03-14 22:12:19 +0900

    modify view application_deadline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
   def create_time(application_deadline)
-    date = application_deadline.chomp
     date = Date.strptime(application_deadline, '%Y年%m月%d日')
     current_date = Date.current
-    (date - current_date).numerator
+    result = (date - current_date).numerator
+
+    if result > 0
+      return result
+    else
+      return 0
+    end
   end
 end
