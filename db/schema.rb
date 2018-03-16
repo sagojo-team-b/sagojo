00ea36e839361dfbb3d06974ba0a3d947c122359
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-16 21:30:46 +0900

    adjustment schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313075415) do
+ActiveRecord::Schema.define(version: 20180314040834) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20180313075415) do
     t.text     "single_word",            limit: 65535
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.integer  "job_tag_id"
+    t.text     "thumbnail",              limit: 65535
+  end
+
+  create_table "job_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "job_tag_type"
   end
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
