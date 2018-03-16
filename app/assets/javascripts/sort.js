6512dcf2f667c797df13815741e6508d933141aa
T.K <>
2018-03-16 14:32:15 +0900

    中断

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -3,6 +3,54 @@ $(document).on('turbolinks:load', function() {
     var html = `<div class="job-thumnail">
                   <a href="/home/job/${article.id}" class="job-thumnail-btn">
                     <img src="${article.thumbnail}" class="job-thumnail__image">
+                    <span class="job-thumnail__content">
+                      <div class="job-thumnail__content--company">
+                        <img src="https://d1te0bovbq6epg.cloudfront.net/common/img/icon-company.svg" class="job-thumnail__content--company-image">
+                        <span class="job-thumnail__content--company-name">
+                          ${article.company}
+                        </span>
+                      </div>
+                      <h3 class="job-thumnail__content--job-title">
+                        ${article.title}
+                      </h3>
+                      <p class="job-thumnail__content--thum-job-desc">
+                        ${article.introduction}
+                      </p>
+                      <p class="job-thumnail__content--thum-job-utility">
+                        募集終了まで
+                        <span class="job-thumnail__content--thum-job-utility remaining-days">
+                        </span>
+                        <span class="job-thumnail__content--thum-job-utility">
+                          日 | 335 view
+                        </span>
+                      </p>
+                      <div class="thum-job-taglist">
+                        <img src="https://d1te0bovbq6epg.cloudfront.net/common/img/icon-tag-01.svg" class="thum-job-taglist__image">
+                        <ul class="thum-job-taglist__ul">
+                          <li>
+                            <img src="https://d1te0bovbq6epg.cloudfront.net/common/img/icon-tag-02.svg" class="thum-job-taglist__ul--image">
+                            <p class="thum-job-taglist__ul--li">
+                              動画
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="thum-job-return">
+                        <div class="inner-01">
+                          <img src="https://d1te0bovbq6epg.cloudfront.net/common/img/icon-money.svg" class="inner-01__image">
+                          <p class="inner-01__p">
+                            ${article.reward}
+                          </p>
+                        </div>
+                        <div class="inner-02">
+                          <% if ${article.other_reward}? %>
+                            <img src="https://d1te0bovbq6epg.cloudfront.net/common/img/icon-object.svg" class="inner-02__image">
+                            <p class="inner-02__p">
+                              他リターンあり
+                          </p>
+                        </div>
+                      </div>
+                    </span
                   </a>
                 </div>`
     return html;
--- app/controllers/header_controller.rb
+++ app/controllers/header_controller.rb
@@ -7,6 +7,13 @@ class HeaderController < ApplicationController
     @job_num = Article.all
     if params[:keyword] == "latest"
       @articles = Article.order("created_at DESC").page(params[:page]).per(10)
+    elsif params[:keyword] == "popular"
+      article_ids = Like.group(:article_id).order('count_article_id DESC').count(:article_id).keys
+      @liked_articles = article_ids.map { |id| Article.find(id) }
+      @articles = Kaminari.paginate_array(@liked_articles).page(params[:page]).per(10)
+    elsif params[:keyword] == "wanted"
+
+    elsif params[:keyword] == "finished"
     else
       @articles = Article.page(params[:page]).per(10)
     end
