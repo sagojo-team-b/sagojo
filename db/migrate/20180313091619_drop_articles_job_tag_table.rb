8e540d74b4f8d928ca18e89e46b7c84b938b5d4b
T.K <>
2018-03-17 00:03:40 +0900

    マイグレーションの整合性の調整

--- db/migrate/20180313091619_drop_articles_job_tag_table.rb
+++
@@ -1,9 +0,0 @@
-class DropArticlesJobTagTable < ActiveRecord::Migration[5.0]
-  def up
-    drop_table :articles_job_tags
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
